edef2eea6870c246ffb85213f9caafd8b7c3b14f
udan-insith <udaninsith23613@gmail.com>
2026-03-08 07:20:28 +0530

    header slide in animation

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-const foodContainer = document.querySelector(.food-container)
+const foodContainer = document.querySelector(".food-container")
+
+const header = document.querySelector("[data-header]")
+
+window.addEventListener("scroll", () => {
+    if (this.window.scrollY >= 100) {
+        header.classList.add("active")
+    } else {
+        header.classList.remove("active")
+    }
+})
